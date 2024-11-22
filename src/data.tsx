8c7bb47a0d4469e40889659505194789f54eddb1
Francine-Pepe <franmelopepe2@gmail.com>
2024-11-22 13:50:07 +0100

    navigation data

---
+++ src/data.tsx
@@ -0,0 +1,32 @@
+export const Navigation = [
+    {
+        id: 0,
+        name: "Biografia",
+        link: "/biografia"
+    },
+    {
+        id: 1,
+        name: "Agenda",
+        link: "/agenda"
+    },
+    {
+        id: 2,
+        name: "Galeria",
+        link: "/galeria"
+    },
+    {
+        id: 3,
+        name: "News",
+        link: "/news"
+    },
+    {
+        id: 4,
+        name: "Vídeos",
+        link: "/videos"
+    },
+    {
+        id: 5,
+        name: "Contato",
+        link: "/contato"
+    },
+]
