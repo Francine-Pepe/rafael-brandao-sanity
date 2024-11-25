90234911701ef7f52842bbf4270d1a135dd17308
Francine-Pepe <franmelopepe2@gmail.com>
2024-11-25 10:46:58 +0100

    carousel data

--- src/data.tsx
+++ src/data.tsx
@@ -1,3 +1,9 @@
+import CarouselImage1 from "../src/assets/01.jpg"
+import CarouselImage2 from "../src/assets/A7C01168-2.jpg"
+import CarouselImage3 from "../src/assets/_RAF0170.png";
+import CarouselImage4 from "../src/assets/_RAF0401.jpg"
+
+
 export const Navigation = [
     {
         id: 0,
@@ -28,5 +34,24 @@ export const Navigation = [
         id: 5,
         name: "Contato",
         link: "/contato"
+    },
+]
+
+export const carouselImages = [
+    {
+        id: 0,
+        image: CarouselImage1
+    },
+    {
+        id: 1,
+        image: CarouselImage2
+    },
+    {
+        id: 2,
+        image: CarouselImage3
+    },
+    {
+        id: 3,
+        image: CarouselImage4
     },
 ]
