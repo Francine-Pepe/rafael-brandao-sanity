42b56ac6ec16ac9fd2469430e20ff8a7849bc70f
Francine-Pepe <franmelopepe2@gmail.com>
2024-11-28 14:28:20 +0100

    footer data

--- src/data.tsx
+++ src/data.tsx
@@ -1,57 +1,71 @@
-import CarouselImage1 from "../src/assets/01.jpg"
-import CarouselImage2 from "../src/assets/A7C01168-2.jpg"
+import CarouselImage1 from "../src/assets/01.jpg";
+import CarouselImage2 from "../src/assets/A7C01168-2.jpg";
 import CarouselImage3 from "../src/assets/_RAF0170.png";
-import CarouselImage4 from "../src/assets/_RAF0401.jpg"
-
+import CarouselImage4 from "../src/assets/_RAF0401.jpg";
+import WhereListen from "./components/WhereListen";
+import FooterContact from "./components/FooterContact";
 
 export const Navigation = [
-    {
-        id: 0,
-        name: "Biografia",
-        link: "/biografia"
-    },
-    {
-        id: 1,
-        name: "Agenda",
-        link: "/agenda"
-    },
-    {
-        id: 2,
-        name: "Galeria",
-        link: "/galeria"
-    },
-    {
-        id: 3,
-        name: "News",
-        link: "/news"
-    },
-    {
-        id: 4,
-        name: "Vídeos",
-        link: "/videos"
-    },
-    {
-        id: 5,
-        name: "Contato",
-        link: "/contato"
-    },
-]
+  {
+    id: 0,
+    name: "Biografia",
+    link: "/biografia",
+  },
+  {
+    id: 1,
+    name: "Agenda",
+    link: "/agenda",
+  },
+  {
+    id: 2,
+    name: "Galeria",
+    link: "/galeria",
+  },
+  {
+    id: 3,
+    name: "News",
+    link: "/news",
+  },
+  {
+    id: 4,
+    name: "Vídeos",
+    link: "/videos",
+  },
+  {
+    id: 5,
+    name: "Contato",
+    link: "/contato",
+  },
+];
 
 export const carouselImages = [
+  {
+    id: 0,
+    image: CarouselImage1,
+  },
+  {
+    id: 1,
+    image: CarouselImage2,
+  },
+  {
+    id: 2,
+    image: CarouselImage3,
+  },
+  {
+    id: 3,
+    image: CarouselImage4,
+  },
+];
+
+export const FooterLinks = [
     {
         id: 0,
-        image: CarouselImage1
+        title: "Onde ouvir",
+        icons: <WhereListen />
     },
     {
         id: 1,
-        image: CarouselImage2
-    },
-    {
-        id: 2,
-        image: CarouselImage3
-    },
-    {
-        id: 3,
-        image: CarouselImage4
+        title: "Contato",
+        icons: <FooterContact />
     },
 ]
