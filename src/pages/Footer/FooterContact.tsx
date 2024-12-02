13b4cc8727fc06e00f7f49fadb2078cb26b395d1
Francine-Pepe <franmelopepe2@gmail.com>
2024-12-02 18:06:24 +0100

    footer contact

---
+++ src/pages/Footer/FooterContact.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Email } from "../../icons/Footer/Email";
+import { Whatsapp } from "../../icons/Footer/Whatsapp";
+
+function FooterContact() {
+  return (
+    <div className="contact-container">
+      <Email />
+      <Whatsapp />
+    </div>
+  );
+}
+
+export default FooterContact;
