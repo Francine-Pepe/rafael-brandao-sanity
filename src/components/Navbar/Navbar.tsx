4063b5f29c01d9f2147d396d88dbc913e6b0f551
Francine-Pepe <franmelopepe2@gmail.com>
2024-12-02 18:02:38 +0100

    function to change the navbar color on scroll

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import NavProps from "./NavProps";
 import { Navigation } from "../../data";
 import { Spotify } from "../../icons/Footer/Spotify";
@@ -6,11 +6,32 @@ import { Instagram } from "../../icons/Instagram";
 import { Youtube } from "../../icons/Footer/Youtube";
 import Logo from "../../icons/Logo";
 import NavbarMobile from "./NavbarMobile";
+import { useLocation } from "react-router-dom";
 
 function Navbar() {
+  const [colorChange, setColorChange] = useState(false);
+  const homeUrl = useLocation().pathname;
+
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 80) {
+      setColorChange(true);
+    } else {
+      setColorChange(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeNavbarColor);
+
   return (
     <>
-      <div className="navbar-container container">
+      <div
+        className={
+          colorChange ? "navbar-container colorChange" : "navbar-container"
+        }
+        style={{
+          backgroundColor: `${homeUrl === "/" ? "transparent" : "#F2F2F2"}`,
+        }}
+      >
         <section className="navigation-logo">
           <Logo />
         </section>
