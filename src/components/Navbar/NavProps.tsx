d41d839f617f97b2886f7723ce8472460a07aa67
Francine-Pepe <franmelopepe2@gmail.com>
2024-12-02 18:03:14 +0100

    uselocation to the navbarprops. It changes the color when outside home route

--- src/components/Navbar/NavProps.tsx
+++ src/components/Navbar/NavProps.tsx
@@ -1,14 +1,23 @@
 import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function NavProps(props: any) {
   const { data } = props;
+  const homeUrl = useLocation().pathname;
 
   return (
     <nav className="navlinks-props">
       {data.map((item: { name: string; link: string }, index: any) => (
         <ul key={index}>
           <li>
-            <NavLink to={item.link}> {item.name} </NavLink>
+            <NavLink
+              to={item.link}
+              style={{
+                color: `${homeUrl === "/" ? "#F2F2F2" : "rgba(32, 0, 15, 1)"}`,
+              }}
+            >
+              {item.name}
+            </NavLink>
           </li>
         </ul>
       ))}
