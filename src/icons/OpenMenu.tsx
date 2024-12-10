787e84a326973fb33b492d79409ed25cfdae0921
Francine-Pepe <franmelopepe2@gmail.com>
2024-12-10 10:00:55 +0100

    condition redering using useLocation in orfer to change the icon color when it´s in a different location

--- src/icons/OpenMenu.tsx
+++ src/icons/OpenMenu.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import type { SVGProps } from "react";
+import { useLocation } from "react-router-dom";
 
 export function OpenMenu(props: SVGProps<SVGSVGElement>) {
+  const homeUrl = useLocation().pathname;
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -10,6 +13,9 @@ export function OpenMenu(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
       className="menu-icon icon"
+      style={{
+        filter: `${homeUrl === "/" ? "brightness(0) saturate(100%) invert(100%) sepia(55%) saturate(1529%) hue-rotate(181deg) brightness(112%) contrast(90%)" : "brightness(0) saturate(100%) invert(8%) sepia(13%) saturate(2785%) hue-rotate(279deg) brightness(94%) contrast(112%)"}`,
+      }}
     >
       <path
         fill="none"
