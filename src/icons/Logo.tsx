b0a514066f25505a0d0aeda594fc370f2f654486
Francine-Pepe <franmelopepe2@gmail.com>
2024-12-10 12:20:12 +0100

    logo condition redering

--- src/icons/Logo.tsx
+++ src/icons/Logo.tsx
@@ -1,13 +1,20 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 /* import RafaLogo from "../assets/rafa-logo-blue.svg"; */
 /* import RafaLogo from "../assets/rafa-logo-blue2.svg"; */
 import RafaLogo from "../assets/rafa-logo-cherry.svg";
+import RafaLogoStroke from "../assets/rafa-logo-cherry-noRect.svg";
 
 function Logo() {
+  const homeUrl = useLocation().pathname;
+
   return (
     <div className="logo">
       <NavLink to="/">
-        <img src={RafaLogo} alt="Rafa Brandao logo" />
+        {homeUrl === "/" ? (
+          <img src={RafaLogo} alt="Rafa Brandao logo" />
+        ) : (
+          <img src={RafaLogoStroke} alt="Rafa Brandao logo" />
+        )}
       </NavLink>
     </div>
   );
