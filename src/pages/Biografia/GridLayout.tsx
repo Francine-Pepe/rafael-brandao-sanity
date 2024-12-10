8e92a251e05b5990721fe87ff411e0c2dc08dbd0
Francine-Pepe <franmelopepe2@gmail.com>
2024-12-10 10:01:29 +0100

    grid layout used on the biografia component

--- src/pages/Biografia/GridLayout.tsx
+++ src/pages/Biografia/GridLayout.tsx
@@ -2,8 +2,11 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Masonry from "@mui/lab/Masonry";
-import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import Modal from "./Modal";
+import { CloseMenu } from "../../icons/CloseMenu";
 
 const Label = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -12,26 +15,64 @@ const Label = styled(Paper)(({ theme }) => ({
 
 export default function Gridlayout(props: any) {
   const { data } = props;
+  console.log(data);
+
+  const [visibleDetails, setVisibleDetails] = useState(false);
+
+  const handleClick = (id: any) => {
+    setVisibleDetails(!visibleDetails && id);
+  };
+
+  const closeModal = () => {
+    setVisibleDetails(true);
+  };
+
   return (
     <Box className="grid-container container">
-      <Masonry columns={{ xs: 1, sm: 2, md: 3}} spacing={2} className="grid-content">
+      <Masonry
+        columns={{ xs: 1, sm: 2, md: 3 }}
+        spacing={2}
+        className="grid-content"
+      >
         {data.map((item: any, index: any) => (
-          <NavLink to={""}>
-            <div key={index} className="grid">
-              <Label className="grid-label">{item.datas}</Label>
-              <img
-                srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
-                src={`${item.img}?w=162&auto=format`}
-                alt={item.alt}
-                loading="lazy"
-                style={{
-                  borderRadius: 4,
-                  display: "block",
-                  width: "100%",
-                }}
-              />
+          <div key={index}>
+            <NavLink to={""}>
+              <div key={item.id} className="grid">
+                <Label className="grid-label">
+                  {item.datas} | {item.country}
+                </Label>
+
+                <img
+                  srcSet={`${item.image}?w=162&auto=format&dpr=2 2x`}
+                  src={`${item.image}?w=162&auto=format`}
+                  alt={item.alt}
+                  loading="lazy"
+                  style={{
+                    borderRadius: 4,
+                    display: "block",
+                    width: "100%",
+                    fill: "red",
+                  }}
+                  className="grid-image"
+                  onClick={() => handleClick(item.id)}
+                />
+              </div>
+            </NavLink>
+            <div onClick={closeModal}>
+              <CloseMenu />
+              {visibleDetails === item.id && (
+                <Modal
+                  datas={item.datas}
+                  text={item.text}
+                  image={item.image}
+                  alt={item.alt}
+                  image1={item.image1}
+                  image2={item.image2}
+                  image3={item.image3}
+                />
+              )}
             </div>
-          </NavLink>
+          </div>
         ))}
       </Masonry>
     </Box>
