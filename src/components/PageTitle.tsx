5cf6af663e5a6a9f321476e7d6f38a68dd4f766c
Francine-Pepe <franmelopepe2@gmail.com>
2025-01-07 17:14:07 +0100

    page title (used as title on the pages)

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 
 function PageTitle(props: any) {
   const { data } = props;
+  const location = useLocation().pathname;
+
   return (
     <section className="page-title-container">
       {data.map((item: any, index: any) => (
         <div key={index} className="title">
-          <h4>{item.title}</h4>
-          <hr />
+          {location === `${item.link}` && (
+            <>
+              <h3>{item.name}</h3>
+              <div className="page-title-bottom-box"></div>
+            </>
+          )}
         </div>
       ))}
     </section>
