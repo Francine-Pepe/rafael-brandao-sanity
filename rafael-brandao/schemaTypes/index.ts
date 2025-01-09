f6ce32de5aa8dff24aa9dc58b3df716a5b3d69c1
Francine-Pepe <franmelopepe2@gmail.com>
2025-01-09 11:06:12 +0100

    biografia cover sanity

--- rafael-brandao/schemaTypes/index.ts
+++ rafael-brandao/schemaTypes/index.ts
@@ -3,5 +3,6 @@ import category from './category'
 import post from './post'
 import author from './author'
 import gallery from './gallery'
+import biografiaCover from './biografiaCover'
 
-export const schemaTypes = [post, author, category, blockContent, gallery]
+export const schemaTypes = [post, author, category, blockContent, gallery, biografiaCover]
