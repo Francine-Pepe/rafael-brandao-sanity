675579ac83c8b75f289c07306719fdaa548f6b9b
Francine-Pepe <franmelopepe2@gmail.com>
2025-01-30 13:01:48 +0100

    text component

---
+++ src/components/Text.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Text(props: any) {
+  const { data } = props;
+  return (
+    <section className="text-container">
+      <div className="text-content">
+        {data.map((item: { text: string; image: string }, index: any) => (
+          <>
+            <p key={index}>{item.text}</p>
+          </>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Text;
