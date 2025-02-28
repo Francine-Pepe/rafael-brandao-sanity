104d8dc3ca0a41b8930af7b0072c29a9c21e8d5b
Francine-Pepe <franmelopepe@gmail.com>
2025-02-28 10:36:50 +0100

    page title translation

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,21 +1,25 @@
+import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router-dom";
 
-function PageTitle(props: any) {
-  const { data } = props;
+function PageTitle() {
   const location = useLocation().pathname;
+  const { t } = useTranslation();
 
   return (
     <section className="page-title-container">
-      {data.map((item: any, index: any) => (
-        <div key={index} className="title">
-          {location === `${item.link}` && (
-            <>
-              <h3>{item.name}</h3>
-              <div className="page-title-bottom-box"></div>
-            </>
-          )}
-        </div>
-      ))}
+      {(t("nav", { returnObjects: true }) as []).map(
+        (item: { name: string; link: string }, index: any) => (
+          <div key={index} className="title">
+            {location === `${item.link}` && (
+              <>
+                <h3>{item.name}</h3>
+                <div className="page-title-bottom-box"></div>
+              </>
+            )}
+          </div>
+        )
+      )}
+
     </section>
   );
 }
