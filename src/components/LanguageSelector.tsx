2f1d6cf88d16abdbf6c2e72e6d6090a450729929
Francine-Pepe <franmelopepe@gmail.com>
2025-02-28 10:36:39 +0100

    language selector used on the mobile menu

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,32 +1,39 @@
 import { useTranslation } from "react-i18next";
+import { useLocation } from "react-router-dom";
 
 const languages = [
-    {code: "pt", language: "PT"},
-    {code: "en", language: "EN"},
-    {code: "de", language: "DE"},
+  { code: "pt", language: "PT" },
+  { code: "en", language: "EN" },
+  { code: "de", language: "DE" },
 ];
 
 const LanguageSelector = () => {
+  const homeUrl = useLocation().pathname;
 
-    const {i18n} = useTranslation();
+  const { i18n } = useTranslation();
 
-    const changeLanguage = (lng: any) => {
-        i18n.changeLanguage(lng)
-    }
+  const changeLanguage = (lng: any) => {
+    i18n.changeLanguage(lng);
+  };
 
-    return (
-        <div className="btn-container">
-            {languages.map((lng) => {
-                return (
-                    <button 
-                        className={lng.code === i18n.language ? "selected-language" : ""}
-                        key={lng.code} onClick={() => changeLanguage(lng.code)}>
-                            {lng.language}
-                    </button>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div className="btn-container">
+      {languages.map((lng) => {
+        return (
+          <button
+            className={lng.code === i18n.language ? "selected-language" : ""}
+            key={lng.code}
+            onClick={() => changeLanguage(lng.code)}
+            style={{
+              color: `${homeUrl === "/" ? "rgb(242, 242, 242" : "rgba(32, 0, 15, 1)"}`,
+            }}
+          >
+            <h3>{lng.language}</h3>
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default LanguageSelector;
