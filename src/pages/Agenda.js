68f9d213f9f47327f8b3395c458fe787754f91fd
Francine-Pepe <franmelopepe@gmail.com>
2025-03-13 20:56:10 +0100

    agenda translation implementation

--- src/pages/Agenda.js
+++ src/pages/Agenda.js
@@ -3,44 +3,60 @@ import client from "../client";
 import { NavLink } from "react-router-dom";
 import Ticket from "../icons/Ticket";
 import PageTitle from "../components/PageTitle";
-import { Navigation } from "../data";
+import { useTranslation } from "react-i18next";
+import { PortableText } from "@portabletext/react";
 
 function Agenda() {
   const [shows, setShows] = useState([]);
+  const { t, i18n } = useTranslation();
 
   useEffect(() => {
-    client
-      .fetch(
-        `*[_type == "post"] { title, date, eventHour, link, description, slug, body, mainImage {
+    const fetchAgenda = async () => {
+      const query = `*[_type == "post"] { title, date, eventHour, link, description, slug, eventTitle, body, mainImage {
           asset -> { _id, url },
           alt
         }
-      }`
-      )
-      .then((data) => setShows(data))
-      .catch(console.error);
+      }`;
+      const result = await client.fetch(query);
+      setShows(result);
+    };
+
+    fetchAgenda();
   }, []);
 
   return (
     <>
-      <PageTitle  />
+      <PageTitle />
       <section className="agenda-container container">
         <div className="agenda-content container">
-          {shows.map((show, index) => (
-            <article key={index}>
-              <div className="agenda-information animate__fadeInUp ">
+          {shows.map((show, index) => {
+            const eventTitle = show.eventTitle || {};
+            const text = show.body || {};
+
+            return (
+              <article key={show.slug.current}>
+                <div className="agenda-information animate__fadeInUp ">
                   <img
                     src={show.mainImage.asset.url}
                     alt={show.title}
                     loading="lazy"
                   />
                   <div className="agenda-information-place-time">
-                    <h3>{show.title}</h3>
+                    <h3>
+                      {eventTitle[i18n.language] ||
+                        eventTitle.pt ||
+                        "No title available"}
+                    </h3>
                     <div className="agenda-date-time">
                       <h3>{show.date}</h3> | <h3>{show.eventHour} horas</h3>
                     </div>
-                    <p>{show.description}</p>
-                    <button style={{display: show.link ? "block" : "none"}}>
+                    <PortableText
+                      value={
+                        text[i18n.language] || text.pt || "No text available"
+                      }
+                    />
+
+                    <button style={{ display: show.link ? "block" : "none" }}>
                       {/* <NavLink to={`/agenda/${show.slug.current}`}>
                     Ver evento
                   </NavLink> */}
@@ -48,16 +64,16 @@ function Agenda() {
                         to={show.link}
                         target="_blank"
                         rel="noopener noreferrer"
-                        
                       >
                         <Ticket />
                         <h4>Tickets</h4>
                       </NavLink>
                     </button>
                   </div>
-              </div>
-            </article>
-          ))}
+                </div>
+              </article>
+            );
+          })}
         </div>
       </section>
     </>
