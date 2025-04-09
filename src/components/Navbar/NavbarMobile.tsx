f75a676219987b088a252bc121aa1d9ef9ace8c6
Francine-Pepe <franmelopepe@gmail.com>
2025-04-09 11:44:38 +0200

    style props added to the navbar mobile

--- src/components/Navbar/NavbarMobile.tsx
+++ src/components/Navbar/NavbarMobile.tsx
@@ -39,7 +39,11 @@ function NavbarMobile() {
         }}
       >
         <div className="mobile-logo">
-          <Logo />
+          <Logo
+            style={{
+              filter: `${homeUrl === "/" ? " brightness(0) saturate(100%) invert(99%) sepia(73%) saturate(60%) hue-rotate(239deg) brightness(116%) contrast(90%)" : "brightness(0) saturate(100%) invert(10%) sepia(55%) saturate(6527%) hue-rotate(313deg) brightness(95%) contrast(100%)"}`,
+            }}
+          />
         </div>
         <div className="mobile-menu">
           {isActive ? <CloseMenu /> : <OpenMenu />}
