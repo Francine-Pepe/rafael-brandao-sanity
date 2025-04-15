e44d0f2969458740770c8ff9f259576df46592a9
Francine-Pepe <franmelopepe@gmail.com>
2025-04-15 12:58:56 +0200

    cookiesProvider wrapping the app in order to acrt globally

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import "./i18n";
-
+import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Suspense fallback="...loading">
-      <App />
-    </Suspense>
+    <CookiesProvider defaultSetOptions={{ path: "/" }}>
+      <Suspense fallback="...loading">
+        <App />
+      </Suspense>
+    </CookiesProvider>
   </React.StrictMode>
 );
 
