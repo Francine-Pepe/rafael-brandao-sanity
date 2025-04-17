7c235677c5d16acf56aefc473586c3194706ef4e
Francine-Pepe <franmelopepe@gmail.com>
2025-04-17 18:10:18 +0200

    clean up

--- src/icons/Logo.tsx
+++ src/icons/Logo.tsx
@@ -1,21 +1,14 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import RafaLogo from "../assets/Rafa-logo-white.svg";
-import RafaLogoCherry from "../assets/Rafa-logo-cherry_.svg";
-/* import RafaLogoStroke from "../assets/rafa-logo-cherry-noRect.svg"; */
+
 
 function Logo(props: any) {
-  const homeUrl = useLocation().pathname;
   const { style } = props;
 
   return (
     <div className="logo">
       <NavLink to="/">
-        {/*         {homeUrl === "/" ? (
-         */}{" "}
         <img src={RafaLogo} alt="Rafa Brandao logo" style={style} />
-        {/* ) : (
-          <img src={RafaLogoCherry} alt="Rafa Brandao logo" />
-        )} */}
       </NavLink>
     </div>
   );
