71666084bf1d056cf047505e6c95094474bf2eec
Francine-Pepe <franmelopepe@gmail.com>
2025-04-17 16:39:30 +0200

    loading div added in suspense

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <CookiesProvider defaultSetOptions={{ path: "/" }}>
-      <Suspense fallback="...loading">
+      <Suspense fallback={<div>Loading...</div>}>
         <App />
       </Suspense>
     </CookiesProvider>
