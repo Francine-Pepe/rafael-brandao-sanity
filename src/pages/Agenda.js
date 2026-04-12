18ea66bcb75a5414fe0da252b4974ef296dad236
Francine-Pepe <franmelopepe@gmail.com>
2026-04-12 19:44:32 +0200

    refactor: organizing date from the most recent to the oldest

--- src/pages/Agenda.js
+++ src/pages/Agenda.js
@@ -52,7 +52,7 @@ function Agenda() {
       <section className="agenda-container container">
         <div className="agenda-content container">
           {shows
-            .sort((a, b) => new Date(a.date) - new Date(b.date))
+            .sort((a, b) => new Date(b.date) - new Date(a.date))
             .map((show) => {
               const eventTitle = show.eventTitle || {};
               const eventDate = show.eventDate || {};
