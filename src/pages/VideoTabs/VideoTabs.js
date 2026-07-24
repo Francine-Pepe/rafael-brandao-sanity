47e04c23d147661eccb290acb8e256ccbcf78165
Francine-Pepe <franmelopepe@gmail.com>
2026-07-24 16:03:50 +0200

    removed not used state

--- src/pages/VideoTabs/VideoTabs.js
+++ src/pages/VideoTabs/VideoTabs.js
@@ -35,7 +35,7 @@ function a11yProps(index) {
 export default function VideoTabs() {
   const [value, setValue] = React.useState(0);
   const [tabs, setTabs] = useState([]);
-  const [key, setKey] = useState(0);
+  const [key] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
